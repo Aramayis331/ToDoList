cc8a716953a89a5dbd7ef3a4efa23316848317c2
Aramayis331 <86958416+Aramayis331@users.noreply.github.com>
2021-10-27 01:18:28 +0400

    aaa

--- js/script.js
+++ js/script.js
@@ -7,7 +7,18 @@ function getToDoList() {
 		return [];
 	}
 }
-getToDoList()
+getToDoList();
+
+function getIndexCheckedLocalSotrage() {
+	let getIndexChecked = localStorage.getItem('taskChecked');
+
+	if(getIndexChecked !== null) {
+		return JSON.parse(getIndexChecked);
+	}else {
+		return [];
+	}
+}
+getIndexCheckedLocalSotrage();
 
 function addToDoList() {
 	let btnAddList = document.querySelector('.btnAddList');
@@ -49,16 +60,33 @@ function refreshList() {
 	let divToDoList = document.querySelector('.divToDoList');
 	if(arrayGetLocalStorageRefresh.length > 0) {
 	let htmlCatalog = '';
-	arrayGetLocalStorageRefresh.map((el) => {
+	let addClasList;
+	
+	arrayGetLocalStorageRefresh.map((el, i) => {
+	let addClastListTrue = true;
+	arrayTaskChecked = getIndexCheckedLocalSotrage();
+
+
+	for(let j = 0; j < arrayTaskChecked.length; j++) {
+		if(el === arrayTaskChecked[j]) {
+			addClasList = 'addClassCheckBox';
+			
+			addClastListTrue = false;
+		}
+	}
+	if(addClastListTrue === true) {
+		addClasList = '';
+	}
 
 		htmlCatalog += `
 
 			<div class='divElToDoList' draggable='true'>
 				<div class='divIconCheckEl'>
-					<input type="checkbox" class="input_checkbox" id="idName${el}" name="zapomnit_signin">
-					<label class="chechbox_label" for="idName${el}"></label>
+					<div class='divChecked'>
+						<i class="fas fa-check iconChecked"></i>
+					</div>
 					<div class='divTextEl'>
-						<p class='textEl'>${el}</p>
+						<p class='textEl ${addClasList}'>${el}</p>
 					</div>
 				</div>
 				<div class='divIconDel'>
@@ -78,27 +106,101 @@ function refreshList() {
 	}
 	del_chechedToDoList();
 }
-refreshList()
+refreshList();
+
 
 function del_chechedToDoList() {
 	let arrayListGetLocalStorage = getToDoList();
 	let iconDel = document.querySelectorAll('.iconDel');
-	let chechbox_label = document.querySelectorAll('.chechbox_label');
+	let divChecked = document.querySelectorAll('.divChecked');
 	let addClassCheckBox = 'addClassCheckBox';
+	let getArrayIndexCheckedLocalSotrage = getIndexCheckedLocalSotrage();
+
 
 	iconDel.forEach((el, i) => {
 		el.addEventListener('click', () => {
-			arrayListGetLocalStorage.splice(i, 1);
+			let delArrayTask = arrayListGetLocalStorage.splice(i, 1);
+			getArrayIndexCheckedLocalSotrage.forEach((el, k) => {
+				if(delArrayTask[0] === el) {
+					getArrayIndexCheckedLocalSotrage.splice(k, 1);
+				}
+			})
+			localStorage.setItem('taskChecked', JSON.stringify(getArrayIndexCheckedLocalSotrage));
 			localStorage.setItem('task', JSON.stringify(arrayListGetLocalStorage));
 			document.querySelector('.divTextEror').style.display = 'none';
 			refreshList();
 		})
 	})
-	chechbox_label.forEach((el , i) => {
+	divChecked.forEach((el , i) => {
 		let textEl = document.querySelectorAll('.textEl');
 		el.addEventListener('click', () => {
 				textEl[i].classList.toggle(addClassCheckBox);
+				index = getArrayIndexCheckedLocalSotrage.indexOf(textEl[i].textContent)
+				if(index === -1) {
+					getArrayIndexCheckedLocalSotrage.push(textEl[i].textContent);
+				}else {
+					getArrayIndexCheckedLocalSotrage.splice(index, 1);
+				}
+				localStorage.setItem('taskChecked', JSON.stringify(getArrayIndexCheckedLocalSotrage));
 		})
 	})
+}
+
+// Drag And Drop //
+
+let divElToDoList = document.querySelectorAll('.divElToDoList');
+let divToDoList = document.querySelector('.divToDoList');
+let dragStartIndex;
+let dragEndIndex;
+let listItems = [];
+
+divElToDoList.forEach((el, i) => {
+	el.addEventListener('dragstart', () => {
+		el.classList.add('dragging');
+		dragStartIndex = i;
+	})
+	el.addEventListener('dragend', () => {
+		el.classList.remove('dragging');
+	})
+	el.addEventListener('dragover', element => {
+		element.preventDefault();
+	})
+	el.addEventListener('dragenter', () => {
+		el.classList.add('hovered');
+	})
+	el.addEventListener('dragleave', () => {
+		el.classList.remove('hovered');
+	})
+	el.addEventListener('drop', () => {
+		el.classList.remove('hovered');
+		dragEndIndex = i;
+
+		spawItems(dragStartIndex, dragEndIndex);
+	})
+})
+
+function spawItems(fromIndex, toIndex) {
+	
+
 
 }
+
+
+// let divElToDoList = document.querySelectorAll('.divElToDoList');
+// let divToDoList = document.querySelector('.divToDoList');
+
+// divElToDoList.forEach((el) => {
+// 	el.addEventListener('dragstart', () => {
+// 		el.classList.add('dragging');
+// 	})
+// 	el.addEventListener('dragend', () => {
+// 		el.classList.remove('dragging');
+// 	})
+// })
+
+// divToDoList.addEventListener('dragover', el => {
+// 	el.preventDefault();
+// 	let classDragging = document.querySelector('.dragging')
+// 	divToDoList.appendChild(classDragging);
+	
+// })
